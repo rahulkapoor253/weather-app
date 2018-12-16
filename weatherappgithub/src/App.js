5b2c68b63936faff5c26cb373d3fed9d3c748573
Rahul Kapoor <rahul253801@gmail.com>
2018-12-17 00:15:10 +0530

    components initialised

--- weatherappgithub/src/App.js
+++ weatherappgithub/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+
+const API_KEY = "d9123fc30ca7f98eb21355a2059b76c7";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=Chandigarh,india&appid=d9123fc30ca7f98eb21355a2059b76c7&units=metric";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form />
       </div>
     );
   }
---
+++ weatherappgithub/src/components/Titles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Temperature Finder</h1>
+        <p>Find out Temperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
