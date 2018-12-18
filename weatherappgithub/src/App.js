be16db2411e4ec69743951dacf876633b793a834
Rahul Kapoor <rahul253801@gmail.com>
2018-12-18 22:39:20 +0530

    data handling completed.

--- weatherappgithub/src/App.js
+++ weatherappgithub/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
+import Weather from './components/Weather';
 
 const API_KEY = "d9123fc30ca7f98eb21355a2059b76c7";
 
@@ -8,6 +9,20 @@ const API_KEY = "d9123fc30ca7f98eb21355a2059b76c7";
 
 
 class App extends Component {
+
+constructor(props) {
+super(props);
+
+this.state = {
+temperature : '',
+humidity : '',
+description : '',
+country : '',
+city : '',
+error : ''
+}
+
+}
 
   //we will make an async call;
 getWeather = async (event) => {
@@ -25,7 +40,33 @@ const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cit
 //we will convert the response;
 const data = await api.json();
 
-console.log(data);
+//check for validations;
+if(city && country) {
+  console.log(data);
+
+  //we will set states;
+  this.setState({
+    temperature : data.main.temp,
+    humidity : data.main.humidity,
+    description : data.weather[0].description,
+    country : data.sys.country,
+    city : data.name,
+  error : ''
+  })
+}
+else {
+  //we will set states back to init and set error val;
+  this.setState({
+    temperature : '',
+    humidity : '',
+    description : '',
+    country : '',
+    city : '',
+  error : "Enter country and city fields."
+  })
+}
+
+
 
 }
 
@@ -34,6 +75,13 @@ console.log(data);
       <div>
         <Titles />
         <Form getWeather = {this.getWeather}/>
+        <Weather temperature={this.state.temperature}
+        country={this.state.country}
+        city={this.state.city}
+        humidity={this.state.humidity}
+        error={this.state.error}
+        description={this.state.description}
+        />
       </div>
     );
   }
---
+++ weatherappgithub/src/components/Weather.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Weather extends React.Component {
+
+    render() {
+        return(
+<div>
+    {this.props.city != '' && this.props.country != '' && <p> Place : {this.props.city}, {this.props.country}</p>}
+
+{ this.props.temperature != '' && <p> Temperature : {this.props.temperature}</p> }
+  {this.props.humidity != '' && <p> Humidity : {this.props.humidity}</p> }
+        { this.props.description != '' && <p> Description : {this.props.description}</p> }
+    {this.props.error!= '' && <p>Error : {this.props.error}</p>}
+</div>
+        );
+    }
+
+}
+
+export default Weather;
